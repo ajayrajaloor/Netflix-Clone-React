9e71787e48ef678a817d2d524713bf44987ccf45
ajayraj1111 <121021527+ajayraj1111@users.noreply.github.com>
2023-08-02 18:22:42 +0530

    Netflix project simple sample finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
+import { action, originals } from './urls'
 import Banner from "./Components/Banner/Banner";
 import Navbar from "./Components/Navbar/Navbar";
 import Rowpost from "./Components/RowPost/Rowpost";
@@ -9,10 +10,13 @@ function App() {
     <div className="App">
     <Navbar/>
     <Banner/>
-    <Rowpost/>
+    <Rowpost url={originals} title= 'Netflix Originals' />
+    <Rowpost url={action} title= 'Action' isSmall />
+    <Rowpost url={action} title= 'Action' isSmall />
     </div>
   );
 }
 
 export default App;
+
  
---
+++ src/Constants/Constants.jsx
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = "34c19caef9a3645bf88f4f8777a0bfe6";
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
